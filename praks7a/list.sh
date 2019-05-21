bfcdf41875a3ec80e7b37ce135a0476af7ffbfe2
Kaspar Koemets <kaspar.koemets@khk.ee>
2019-05-21 08:50:49 +0300

    skript

--- praks7a/list.sh
+++ praks7a/list.sh
@@ -3,10 +3,24 @@
 #kataloogi andmed
 echo "Millise kataloogi sisu  soovite vaadata?"
 read directory
-names=$(ls -l $directory | awk '{if(NR>1)print $9}')
-types=$(ls -l $directory | awk '{if(NR>1)print substr ($0, 0, 2)}')
+name=$(ls -l $directory | awk '{if(NR>1)print $9}')
+type=$(ls -l $directory | awk '{if(NR>1)print substr ($0, 0, 2)}')
 num=1
+faile=0
+kauste=0
+linke=0
 for type in $types; do
 if [ type =='-' ]; then
 echo -n 'fail: '
-elif [ $type ==
+let faile++
+elif [ $type == 'd' = ]; then
+echo -n 'kaust'
+let kauste++
+elif [ $type == '1' ]; then
+echo -n 'link: '
+let linke++
+fi
+echo $name | cut -d ' ' -f$num
+let num++
+done
+echo "Kaustas on $faile faile, $kauste kausta ja $linke linki."
